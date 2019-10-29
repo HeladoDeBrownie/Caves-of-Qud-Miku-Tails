90756780acedd6808ee814be05ba3313bf092a21
helado de brownie <helado.de.brownie@gmail.com>
2019-10-29 14:53:02 -0400

    make miku tails select melee targets

---
+++ helado_MikuInfection.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using XRL.Rules;
+
+namespace XRL.World.Parts
+{
+    public class helado_MikuInfection : IPart
+    {
+        public GameObject GetTarget()
+        {
+            var possibleTargets = new List<GameObject>();
+
+            ParentObject.GetCurrentCell().ForeachLocalAdjacentCell(delegate (Cell Cell)
+            {
+                var combatTarget = Cell.GetCombatTarget(ParentObject);
+
+                if (combatTarget != null)
+                {
+                    possibleTargets.Add(combatTarget);
+                }
+            });
+
+            var count = possibleTargets.Count;
+
+            return count == 0 ?
+                null :
+                possibleTargets[Stat.Rnd.Next(0, count)];
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            switch (E.ID)
+            {
+                case "EndTurn":
+                    var target = GetTarget();
+
+                    if (target != null)
+                    {
+                        AddPlayerMessage("TODO: attack " + target.DisplayName);
+                    }
+
+                    return true;
+
+                default:
+                    return base.FireEvent(E);
+            }
+        }
+
+        public override void Register(GameObject Object)
+        {
+            Object.RegisterPartEvent(this, "EndTurn");
+            base.Register(Object);
+        }
+    }
+}
