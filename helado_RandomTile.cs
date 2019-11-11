149eedf49a9042194017c98d2d7ec5f58227301d
helado de brownie <helado.de.brownie@gmail.com>
2019-11-11 02:04:17 -0500

    remove extraneous log messages

--- helado_RandomTile.cs
+++ helado_RandomTile.cs
@@ -6,15 +6,10 @@ namespace XRL.World.Parts
 
         public override void Register(GameObject Object)
         {
-            Log("Randomizing tile...");
             var Tile = Choices.Split(',').GetRandomElement();
-            Log("We picked " + Tile + "!");
             Object.pRender.Tile = Tile;
-            Log("It's been set.");
             base.Register(Object);
-            Log("Base registration is done.");
             Object.RemovePart(this);
-            Log("We should be removed now.");
         }
     }
 }
